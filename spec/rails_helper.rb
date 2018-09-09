38fffecfac94ea55e1df6d2ec67029b43629e0b5
Tobias Andre <tobiasae@gmail.com>
2018-09-08 21:41:32 -0300

    Adding shoulda matcher for user specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean.with(:truncation)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.around(:each) do |example|
