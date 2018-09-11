0dfe127dbdb722de1aa1b0bc34a764477a22348b
Tobias Andre <tobiasae@gmail.com>
2018-09-11 15:21:16 -0300

    Refactoring the authorization and adding user_signer_in? method

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -44,6 +44,22 @@ RSpec.describe Authenticable do
 			end
 		end
 	end
+
+	describe '#user_logged_in?' do
+		context 'when there is a logged in' do
+			before do
+				user = create(:user)
+				allow(app_controller).to receive(:current_user).and_return(user)
+			end
+			it { expect(app_controller.user_logged_in?).to be true }
+		end
+		context 'when there is no user logged in' do
+			before do
+				allow(app_controller).to receive(:current_user).and_return(nil)
+			end
+			it { expect(app_controller.user_logged_in?).to be false }
+		end
+	end
 end
 
 #def current_user
